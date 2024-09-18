9c42ca4daaa7e09dd7a21ca911a1026b7ce20f43
User Panpetos <petr.vakulskiy88@gmail.com>
2024-09-18 05:12:13 +0300

    Обновление дизайна главной страницы

--- src/deploy.sh
+++ src/deploy.sh
@@ -6,15 +6,18 @@ git commit -m "$1"
 git push origin master
 
 # Шаг 2: SSH подключение и деплой на сервере
-ssh -T username@server_ip << 'EOF'
+ssh -T root@brandingsite.store << 'EOF'
   cd Fisherman_cash/Fisherman_cash/my-multiplayer-app
   git pull origin master
   pm2 restart 5
-  echo "Деплой завершен!"
+  echo "Деплой на сервере завершен!"
 EOF
 
 # Шаг 3: Локальная сборка проекта
 npm run build
+
+# Шаг 4: Деплой на Netlify
+netlify deploy --prod --dir=build
 
 # Сообщение об успешном завершении
 echo "Все этапы деплоя завершены!"
