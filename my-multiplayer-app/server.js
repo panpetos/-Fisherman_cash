73ea0da388e4ca056c1f0c044aaed3e90af747b2
User Panpetos <petr.vakulskiy88@gmail.com>
2024-09-22 08:14:23 +0300

    Обновление дизайна главной страницы

--- my-multiplayer-app/server.js
+++ my-multiplayer-app/server.js
@@ -22,10 +22,11 @@ const players = {}; // Хранение данных о всех игроках
 io.on('connection', (socket) => {
   console.log('Новый игрок подключился:', socket.id);
 
-  // Инициализируем нового игрока с дефолтной позицией и анимацией
+  // Инициализируем нового игрока с дефолтной позицией, анимацией и углом поворота
   players[socket.id] = {
     id: socket.id,
     position: [0, 0, 0],
+    rotation: 0, // Новый игрок начинает с углом поворота 0
     animation: 'Idle', // Стандартная анимация
   };
 
@@ -39,6 +40,7 @@ io.on('connection', (socket) => {
   socket.on('playerMove', (data) => {
     if (players[socket.id]) {
       players[socket.id].position = data.position;
+      players[socket.id].rotation = data.rotation; // Обновляем угол поворота
       players[socket.id].animation = data.animation; // Обновляем анимацию
       io.emit('updatePlayers', players); // Передаем обновленные данные всем игрокам
     }
